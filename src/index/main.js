f62915f79cf7ddefe5914c02b56e12229d720cb4
Sky <sshirasshmu@gmail.com>
2021-08-08 09:15:53 +0300

    commit

--- src/index/main.js
+++ src/index/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from '.././store'
-import $ from 'jquery'
+// import $ from 'jquery'
 window.$ = window.jQuery = require('jquery');
 import VCalendar from 'v-calendar';
 
-import 'babel-polyfill'
-// import '@/plugins/vuetify'
 
 createApp(App).use(store).use(VCalendar, {}).mount('#app');
